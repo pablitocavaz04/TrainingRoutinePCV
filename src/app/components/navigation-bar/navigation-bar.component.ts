bd0a72944021214c45bb5b0e149cfa658f8fb1bd
pablito04 <pablocavaz2004@gmail.com>
2025-01-28 02:23:18 +0100

    Just Responsive Butoon in navbar

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -12,26 +12,35 @@ export class NavigationBarComponent implements OnInit {
   menuOpen = false;
   isScrolled = false;
   imagenPerfil: string | null = null;
+  isLanguageMenuOpen = false; // Controla la apertura del menú de idioma
+  currentLanguage = 'es'; // Idioma actual
 
-  constructor(private authService: AuthService,
-               private translationService: TranslationService) {}
-
-  changeLanguage(lang: string) {
-    this.translationService.setLanguage(lang);
-  }
+  constructor(private authService: AuthService, private translationService: TranslationService) {}
 
   ngOnInit(): void {
     this.cargarImagenPerfil();
+    this.currentLanguage = this.translationService.getCurrentLanguage(); // Obtener el idioma guardado
   }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
 
+  toggleLanguageMenu() {
+    this.isLanguageMenuOpen = !this.isLanguageMenuOpen; // Abrir/cerrar menú de idiomas
+  }
+
+  changeLanguage(lang: string) {
+    this.currentLanguage = lang; // Actualizar idioma actual
+    this.translationService.setLanguage(lang); // Cambiar idioma en el servicio
+    this.isLanguageMenuOpen = false; // Cerrar menú de idiomas
+  }
+
   cargarImagenPerfil() {
     this.authService.getUsuarioLogueado().subscribe({
       next: (usuario) => {
-        this.imagenPerfil = usuario?.persona?.perfil?.formats?.thumbnail?.url || usuario?.persona?.perfil?.url || null;
+        this.imagenPerfil =
+          usuario?.persona?.perfil?.formats?.thumbnail?.url || usuario?.persona?.perfil?.url || null;
       },
       error: (err) => {
         console.error('Error al obtener la imagen del usuario:', err);
